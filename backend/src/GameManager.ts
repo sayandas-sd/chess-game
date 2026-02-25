c14a69310ed09cf027059d676e651b8598788cc7
sayandas-sd <sayandasmac@gmail.com>
2026-02-25 18:11:05 +0530

    fix(move): player move playload.move

--- backend/src/GameManager.ts
+++ backend/src/GameManager.ts
@@ -41,7 +41,7 @@ export class GameManager {
             if(message.type === MAKE_MOVE) {
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);   
                 if(game) {
-                    game.makeMove(socket, message.move);
+                    game.makeMove(socket, message.payload.move);
                 }
             }
         });
