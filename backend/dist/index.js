24692dc8add37c2b9608722f5d5f76da40ddcfae
sayandas-sd <sayandasmac@gmail.com>
2026-03-16 23:27:46 +0530

    fix: set hardcoded port to dotenv

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,12 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const GameManager_1 = require("./GameManager");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const dotenv_1 = __importDefault(require("dotenv"));
+const http_1 = require("http");
+dotenv_1.default.config();
+const PORT = process.env.PORT || 8080;
+const server = (0, http_1.createServer)();
+const wss = new ws_1.WebSocketServer({ server });
 const gamemanager = new GameManager_1.GameManager();
 wss.on('connection', function connection(ws) {
     gamemanager.addUserToGame(ws);
-    ws.on('disconnect', () => {
+    ws.on('close', () => {
         gamemanager.removeUserFromGame(ws);
     });
+});
+server.listen(PORT, () => {
+    console.log("server running on port", PORT);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
+import dotenv from "dotenv";
+import { createServer} from "http";
 
-const wss = new WebSocketServer({ port: 8080 });
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const server = createServer();
+
+const wss = new WebSocketServer({ server });
 
 const gamemanager = new GameManager();
 
@@ -10,8 +18,12 @@ wss.on('connection', function connection(ws) {
 
  gamemanager.addUserToGame(ws);
 
- ws.on('disconnect', () => {
+ ws.on('close', () => {
     gamemanager.removeUserFromGame(ws);
   });
 
+});
+
+server.listen(PORT, () => {
+  console.log("server running on port", PORT);
 });
